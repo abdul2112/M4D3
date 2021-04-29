8309917707cc759a0f092eaa9cb6b37adb6320e1
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-04-29 14:08:45 +0100

    added warningSign, singleBook and myBadge

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import MyNavBar from './components/MyNavBar';
 import MyJumbotron from './components/MyJumbotron';
 import LatestReleases from './components/LatestReleases';
 import MyFooter from './components/MyFooter';
+import WarningSign from './components/WarningSing';
+import MyBadge from './components/MyBadge';
+import SingleBook from './components/SingleBook';
+import fantasy from './data/fantasy.json';
 
 function App() {
   return (
     <div>
       <MyNavBar title="Bookstore" />
+      <WarningSign text="WARNING!!!" />
+      <MyBadge text="NEW!!!" color="danger" />
       <MyJumbotron />
+      <SingleBook book={fantasy[0]} />
       <LatestReleases />
       <MyFooter />
     </div>
